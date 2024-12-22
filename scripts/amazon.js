8da9a5b8a5b221fba75b7a7706d1d847b38f77c4
YessetHumanMan <eset.bimanov@gmail.com>
2024-12-22 19:02:08 +0500

    feat: add data folder from amazon.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,4 @@
 
-const products = [
-	{
-	  images: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-	  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-	  rating: {
-	     stars: 4.5,
-	     count: 87
-	  },
-	  priceCent:1090 
-	},
-	{
-	  images: "images/products/intermediate-composite-basketball.jpg",
-	  name: "Intermediate Size Basketball",
-	  rating: {
-	     stars: 4,
-	     count: 127
-	  },
-	  priceCent: 2095
-	},
-	{
-	  images: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-	  name: "Adults Plain Cotton T-Shirt - 2 Pack",
-	  rating: {
-	     stars: 4.5,
-	     count: 56
-	  },
-	  priceCent: 799
-	}
-]
-
 let productHTML = '';
 const productsGrid = document.querySelector(".products-grid")
 
@@ -36,7 +6,7 @@ products.forEach((product) => {
 
    productHTML += `<div class="product-container">
           <div class="product-image-container">
-            <img class="product-image" src="${product.images}">
+            <img class="product-image" src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
@@ -51,7 +21,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-           $${product.priceCent / 100}
+           $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
