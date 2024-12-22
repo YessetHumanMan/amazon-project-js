7e964ac273756efcb83eacb36abf35cce1c9b04b
YessetHumanMan <eset.bimanov@gmail.com>
2024-12-22 12:12:08 +0500

    feat: add new file amazon.js

---
+++ scripts/amazon.js
@@ -0,0 +1,33 @@
+const products = [
+	{
+	  images: "images/products/athletic-cotton-socks-6-pairs.jpg",
+	  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+	  rating: {
+	     stars: 4.5,
+	     count: 87
+	  },
+	  praceCent:1090 
+	},
+	{
+	  images: "images/products/intermediate-composite-basketball.jpg",
+	  name: "Intermediate Size Basketball",
+	  rating: {
+	     stars: 4,
+	     count: 127
+	  },
+	  proceCent: 2095
+	},
+	{
+	  images: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+	  name: "Adults Plain Cotton T-Shirt - 2 Pack",
+	  rating: {
+	     stars: 4.5,
+	     count: 56
+	  },
+	  priceCent: 799
+	}
+]
+
+products.forEach((product) => {
+  const html = ``
+})
