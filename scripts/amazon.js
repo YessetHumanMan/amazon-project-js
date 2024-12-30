53d8c2b1dcf54dcec6294ad7800c7a7ba0ccc130
YessetHumanMan <eset.bimanov@gmail.com>
2024-12-30 14:59:27 +0500

    feat: add new function delete product into cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js"
+mport { cart, addToCart } from "../data/cart.js"
 import { products } from "../data/products.js";
 import { moneyToFixed } from "./utils/money.js"
 let productsHTML = '';
