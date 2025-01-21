f701c1597881df9196abac31f3adbb4c4dbb0465
YessetHumanMan <eset.bimanov@gmail.com>
2025-01-21 20:32:44 +0500

    feat: add create function Cart

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -1,3 +1,4 @@
+function Cart() {
 const cart = {
 	cartItem: undefined,
 init() {
@@ -72,85 +73,12 @@ this.cartItem.forEach((cartItem) => {
 },
 
 }
-cart.init()
-console.log(cart);
-
-const businessCart = {
-		cartItem: undefined,
-init() {
-	 this.cartItem = JSON.parse(localStorage.getItem("cart-oop"))
-
-if (!this.cartItem) {
-  this.cartItem = [
-	{
-	 productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-	 quantity: 2,
-	 deliveryOptionId: "1"
-	},
-	{
-	 productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-	 quantity: 1,
- 	 deliveryOptionId: "2"
-	}
-]; 
+return cart
 }
-},
-
- saveToStorage() {
-	localStorage.setItem("cart-oop", JSON.stringify(this.cartItem))
-},
-
- addToCart(productId) {
-	let machingItem
-	this.cartItem.forEach((cartItem) => {
-	if (productId === cartItem.productId) {
-	  machingItem = cartItem
-      }
-	});
-
-	if (machingItem) {
-	  machingItem.quantity += 1
-     } else {
-	 this.cartItem.push({
-    	 productId: productId,
-	 quantity: 1,
-	 deliveryOptionId: "1"
-      })
-     }
-	this.saveToStorage()
-},
 
 
- removeCartItem(productId) {
- this.cartItem = this.cartItem.map(cartItem => {
-    if (cartItem.productId === productId) {
-      if (cartItem.quantity > 1) {
-        return { ...cartItem, quantity: cartItem.quantity - 1 };
-      }
-      // Если количество равно 1, товар будет удален
-      return null;
-    }
-    return cartItem;
-  }).filter(cartItem => cartItem !== null);
+const cart = Cart()
+const businessCart = Cart()
 
-  this.saveToStorage();
-},
-
-
-updateDeliveryOptions(productId, deliveryOptionId) {
-let machingItem
-this.cartItem.forEach((cartItem) => {
-  if (productId === cartItem.productId) {
-	machingItem = cartItem
-  }
-})
-	machingItem.deliveryOptionId = deliveryOptionId
-	this.saveToStorage()
-},
-
-
-
-};
-
-businessCart.init();
+console.log(cart)
 console.log(businessCart)
